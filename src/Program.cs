4576562e8950c1c9ef27c58faf0c570f3a3c65f5
Thomas <fenotthomas@gmail.com>
2024-07-01 16:32:56 +0200

    pass stage

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 
+
 if (args.Length < 1)
 {
     Console.WriteLine("Please provide a command.");
@@ -16,6 +17,14 @@ if (command == "init")
     Directory.CreateDirectory(".git/refs");
     File.WriteAllText(".git/HEAD", "ref: refs/heads/main\n");
     Console.WriteLine("Initialized git directory");
+}
+else if (command == "cat-file")
+{
+    var toto = args;
+    Console.WriteLine($"Arg 1 : {args[1]}");
+    Console.WriteLine($"Arg 2 : {args[2]}");
+
+
 }
 else
 {
