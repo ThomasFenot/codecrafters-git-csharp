02d21eac010a79d435b62a3a8ba1636d154407cf
Thomas <fenotthomas@gmail.com>
2024-07-01 17:23:27 +0200

    pass stage

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
 
 
 if (args.Length < 1)
@@ -23,7 +25,8 @@ else if (command == "cat-file")
     var fileNameArgument = args[2];
     var folder = fileNameArgument[2..];
     var fileName = fileNameArgument[..2];
-
+    var path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+    Console.WriteLine($"Path is : {path}");
     string readText = File.ReadAllText($".git/objects/{folder}/{fileName}");
     Console.WriteLine(readText);
 }
